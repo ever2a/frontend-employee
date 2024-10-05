4e3addf07604013590df6d502312beca3546cc02
Ever2A <ever.arones.ayala@gmail.com>
2024-10-05 13:27:29 -0500

    Layout and employee part 1

--- src/app/pages/employee/employee.component.ts
+++ src/app/pages/employee/employee.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
+
+  isFormVisible = signal<boolean>(false);
 
 }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  loginObj: any = {
+    username: '',
+    password: ''
+  };
 
+  router = inject(Router);
+
+  login() {
+    if(this.loginObj.username === 'admin' && this.loginObj.password === 'admin') {
+      this.router.navigateByUrl('/dashboard');
+    } else {
+      alert('Invalid credentials');
+    }
+  }
 }
